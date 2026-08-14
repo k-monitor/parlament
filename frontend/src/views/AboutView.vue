7a3ece0de48beaeeae20f6f0459b833f0a1d177f
boa <boa@protonmail.com>
2026-08-14 19:51:46 +0200

    add further sources

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -17,6 +17,25 @@ import DonateCard from '../components/DonateCard.vue'
     <p v-html="$t('about.dataBody1')"></p>
     <p v-html="$t('about.dataBody2')"></p>
     <p v-html="$t('about.dataBody3')"></p>
+
+    <h2>{{ $t('about.sourcesTitle') }}</h2>
+    <p v-html="$t('about.sourcesBody1')"></p>
+    <p v-html="$t('about.sourcesBody2')"></p>
+    <p v-html="$t('about.sourcesBody3')"></p>
+    <p>{{ $t('about.sourcesModelsIntro') }}</p>
+    <ul>
+      <li>{{ $t('about.sourcesModel1') }}</li>
+      <li>{{ $t('about.sourcesModel2') }}</li>
+      <li>{{ $t('about.sourcesModel3') }}</li>
+      <li>{{ $t('about.sourcesModel4') }}</li>
+      <li>{{ $t('about.sourcesModel5') }}</li>
+    </ul>
+    <p v-html="$t('about.sourcesBody4')"></p>
+    <p v-html="$t('about.sourcesBody5')"></p>
+    <p v-html="$t('about.sourcesBody6')"></p>
+    <p v-html="$t('about.sourcesBody7')"></p>
+    <p v-html="$t('about.sourcesBody8')"></p>
+    <p v-html="$t('about.sourcesBody9')"></p>
   </article>
 
   <DonateCard class="about-donate" />
@@ -29,5 +48,6 @@ import DonateCard from '../components/DonateCard.vue'
    global link style drops the underline, which reads poorly mid-sentence).
    :deep() is required because the anchors come from v-html. */
 .prose p { line-height: 1.65; margin: 0 0 .9rem; }
+.prose ul { line-height: 1.65; margin: -.4rem 0 .9rem; padding-left: 1.25rem; }
 .prose :deep(a) { text-decoration: underline; text-underline-offset: 2px; }
 </style>
